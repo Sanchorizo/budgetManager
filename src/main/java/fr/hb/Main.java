23700805df1950b521b2f095946b9503583f3fc4
Sanchorizo <maxime--martinez@hotmail.fr>
2024-10-07 16:24:25 +0200

    add expanse + total calculus

--- src/main/java/fr/hb/Main.java
+++ src/main/java/fr/hb/Main.java
@@ -1,7 +1,20 @@
 package fr.hb;
 
 public class Main {
+
     public static void main(String[] args) {
+        BudgetManager budgetManager = new BudgetManager();
+
         System.out.println("Bienvenue dans le gestionnaire de budget!!");
+        budgetManager.addExpense("Courses", 50.0f);
+        budgetManager.addExpense("Tranport", 15.0f);
+
+        budgetManager.calculateTotal();
+
     }
+
+
+
+
+
 }
